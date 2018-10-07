c717c2dfe05a258cd1a9f5ff3188ba56c21dfd00
Mateusz Małecki <mmateusz314@gmail.com>
2018-10-07 14:52:11 +0200

    w sumie niewielie

---
+++ include/ECS.h
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Component{
+private:
+
+public:
+  virtual void Start();
+  virtual void Update();
+};
+
+class GameObject{
+private:
+  bool isActve = true;
+  std::vector<Component*> components;
+public:
+  void Start();
+  void Update()
+  {
+    for (int i = 0 ; i < static_cast<int>(components.size()); i++)
+    {
+      components[i]->Update();
+    }
+  }
+
+};
