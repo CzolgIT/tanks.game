2e7b33f7ff876f76fc1360951d8744f28b58a2b4
Andrzej <andrzej@Andrzejs-iMac.local>
2018-10-08 22:55:53 +0200

    repaired includes for macOS

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 #include <SDL2/SDL.h>
+#ifdef __APPLE__
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_net/SDL_net.h>
+#else
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_net.h>
+#endif
 #include "ECS.h"
 #include "Game.h"
 #include "Texture.h"
