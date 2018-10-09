8e04b08ea4bd160b4b8abdc029c29672ceaa9b4d
Andrzej <andrzej@Andrzejs-iMac.local>
2018-10-09 17:25:59 +0200

    created Sprite class header

--- include/Sprite.h
+++ include/Sprite.h
@@ -1,1 +1,19 @@
+#ifndef Sprite_h
+#define Sprite_h
+
+class Sprite
+{
+public:
+    bool setTexture(Texture texture);
+    bool setRect( int x, int y, int w, int h ); // start point on texture and size
+    bool draw(SDL_Renderer* renderer, int x, int y, int angle = 0); // start point on window
+    bool setCenter( int x, int y); // to rotate image around the point
+private:
+    Texture texture;
+    SDL_Rect* clip = NULL;
+    int centerX = 0;
+    int centerY = 0;
+};
+
+#endif
 
