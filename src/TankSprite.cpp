077190db9b22737da3449152985641dc8ef00748
Mateusz Małecki <mmateusz314@gmail.com>
2018-11-13 20:01:10 +0100

    Fixed movement animation

--- src/TankSprite.cpp
+++ src/TankSprite.cpp
@@ -4,22 +4,22 @@ TankSprite::TankSprite( SDL_Renderer* r , int color )
 {
     renderer = r;
     Texture* texture = new Texture( renderer , "assets/tex_tank.png" );
- 
+
     spriteTrackLeft[0] = new Sprite( renderer , texture , 96+122+122 , 0 , 33 , 158 , 66 , 80 );
     spriteTrackRight[0] = new Sprite( renderer , texture , 96+122+122 , 0 , 33 , 158 , -33 , 80 );
-    
+
     spriteTrackLeft[1] = new Sprite( renderer , texture , 96+122+122+33 , 0 , 33 , 158 , 66 , 80 );
     spriteTrackRight[1] = new Sprite( renderer , texture , 96+122+122+33 , 0 , 33 , 158 , -33 , 80 );
-    
+
     spriteTrackLeft[2] = new Sprite( renderer , texture , 96+122+122+33+33 , 0 , 33 , 158 , 66 , 80 );
     spriteTrackRight[2] = new Sprite( renderer , texture , 96+122+122+33+33 , 0 , 33 , 158 , -33 , 80 );
-    
+
     spriteTrackLeft[3] = new Sprite( renderer , texture , 96+122+122+33+33+33 , 0 , 33 , 158 , 66 , 80 );
     spriteTrackRight[3] = new Sprite( renderer , texture , 96+122+122+33+33+33 , 0 , 33 , 158 , -33 , 80 );
-    
+
     spriteTrackLeft[4] = new Sprite( renderer , texture , 96+122+122+33+33+33+33 , 0 , 33 , 158 , 66 , 80 );
     spriteTrackRight[4] = new Sprite( renderer , texture , 96+122+122+33+33+33+33 , 0 , 33 , 158 , -33 , 80 );
-    
+
     if ( color == 1 )
     {
         spriteBody = new Sprite( renderer , texture , 96 , 0 , 122 , 171 , 61 , 84 );
@@ -49,20 +49,26 @@ TankSprite::TankSprite( SDL_Renderer* r , int color )
 
 void TankSprite::draw( int x , int y , int dir , int dirT , int move )
 {
-    if ( move < 0 )
+    if (step >= 14 || step <= -14)
     {
-        step++;
-        if (step == 15)
-        {
-            step = 0;
-        }
+        step = 0;
     }
-    else
+
+    if (move > 0){
+        step++;
+        spriteTrackLeft[4-(int)round(abs(step)/3)]->draw( x-66 , y-105+25 , dir);
+        spriteTrackRight[4-(int)round(abs(step)/3)]->draw( x-66+99, y-105+25 , dir);
+    }else if (move < 0) {
+        step --;
+        spriteTrackLeft[(int)round(abs(step)/3)]->draw( x-66 , y-105+25 , dir);
+        spriteTrackRight[(int)round(abs(step)/3)]->draw( x-66+99, y-105+25 , dir);
+    } else
     {
         step = 0 ;
+        spriteTrackLeft[0]->draw( x-66 , y-105+25 , dir);
+        spriteTrackRight[0]->draw( x-66+99, y-105+25 , dir);
     }
-    spriteTrackLeft[(int)round(step/3)]->draw( x-66 , y-105+25 , dir);
-    spriteTrackRight[(int)round(step/3)]->draw( x-66+99, y-105+25 , dir);
+
     spriteBody->draw( x-66+5, y-105+21 , dir);
     spriteTower->draw( x-66+18, y-105+41 , dirT);
     spriteBarrel->draw( x-66+39, y-105 , dirT);
