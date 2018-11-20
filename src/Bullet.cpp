de4dea780edc4f744175e00cc88a65ff7f3512fb
Mateusz Małecki <mmateusz314@gmail.com>
2018-11-21 00:23:30 +0100

    Added bullet boundaries

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -21,6 +21,16 @@ Collider Bullet::collider() {
 
 void Bullet::move( float timeStep )
 {
-x -= (cos(direction *M_PI/180) * BULLETSPEED * timeStep);
-y -= (sin(direction *M_PI/180) * BULLETSPEED * timeStep);
+    x -= (cos(direction *M_PI/180) * BULLETSPEED * timeStep);
+    y -= (sin(direction *M_PI/180) * BULLETSPEED * timeStep);
+
+    if( x < (int)((double)width/2) ||
+        x > 2048 - (int)((double)width/2) ||
+        y < (int)((double)height/2) ||
+        y > 2048 - (int)((double)height/2)
+        )
+    {
+        this->setToBeDestroyed();
+    }
+
 }
